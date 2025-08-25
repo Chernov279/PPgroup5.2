5d7d9437cce19f0631cd5b7b697e502fe1ebe7a3
Chernov279 <146570140+Chernov279@users.noreply.github.com>
2025-08-25 21:06:59 +0300

    ref(healthcheck): added logger for healthcheck dependencies, added documentation

--- src/healthcheck/healthcheck_router.py
+++ src/healthcheck/healthcheck_router.py
@@ -1,12 +1,15 @@
+from asyncio import gather
+
 from fastapi import APIRouter, status
 from src.healthcheck.dependencies import check_kafka, check_redis, check_postgres
+from src.kafka.consumers import kafka_consumers
 
 healthcheck = APIRouter(prefix="/health", tags=["Healthcheck"])
 
 
 @healthcheck.get("/kafka", status_code=status.HTTP_200_OK)
 async def health_kafka():
-    await check_kafka()
+    await check_kafka(kafka_consumers)
     return {"kafka": "ok"}
 
 
@@ -24,7 +27,9 @@ async def health_postgres():
 
 @healthcheck.get("/all", status_code=status.HTTP_200_OK)
 async def health_all():
-    await check_kafka()
-    await check_redis()
-    await check_postgres()
+    await gather(
+        check_kafka(kafka_consumers),
+        check_redis(),
+        check_postgres()
+    )
     return {"status": "ok"}
